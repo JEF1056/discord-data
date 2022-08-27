4634aa4ada697f006925d38b79c53d8a30ff5d27
Jess Fan <jessefan2002@gmail.com>
2022-08-27 16:30:34 -0700

    initial commit

---
+++ clean.py
@@ -0,0 +1,370 @@
+import os
+import math
+import ijson
+import dataset
+from tqdm import tqdm
+import multiprocessing
+import concurrent.futures
+from src.helpers import clean
+
+data_folder = "data"
+cache_folder = "cleaned"
+processing_cores = math.ceil(multiprocessing.cpu_count() * 0.6875)
+print(f"{processing_cores} cores for workers, {multiprocessing.cpu_count()-processing_cores} cores for db")
+
+db_url = "postgresql://admin:r3wyhgifqloprm9u0iqh4t89qu2evrq2gie8rd9uj23rlhf8q3uyghfvcjm2pw@localhost:5432/postgres"
+
+def table_ok(table):
+    with dataset.connect(db_url) as db:
+        status = db['status'].find_one(ref=table)
+        if status and dict(status)["ok"]:
+            return True
+    return False
+
+def chunk(arr, arr_len, n):
+    arr = list(arr)
+    chunk_size = math.ceil(arr_len/n)
+    return (arr[i:i+chunk_size] for i in range(0, arr_len, chunk_size)), math.ceil(arr_len/chunk_size)
+
+def cache_users(path):
+    messages_processed = 0
+    names = []
+    personas = []
+
+    with open(os.path.join(data_folder, "intros", path), "r") as f, dataset.connect(db_url) as db:
+        messages = ijson.items(f, 'messages.item')
+        for message in messages:
+            # Ignore if content is:
+            # - too short
+            # - has an embed
+            # - is a non-default message
+            # - author is a bot
+            # - author's name is too long
+            if (len(message["content"]) < 100 or
+                len(message["content"]) > 900 or
+                message["embeds"] != [] or
+                message["type"] != "Default" or
+                message["author"]["isBot"] == True or
+                message["author"]["name"] == "Deleted User" or 
+                len(message["author"]["name"]) > 100):
+                continue
+
+            cleaned_name = clean(message["author"]["name"])
+            cleaned_message = clean(message["content"])
+
+            if not (cleaned_message and cleaned_name):
+                continue
+
+            # Process in memory to increase speed
+            if len(cleaned_name) < 100:
+                names.append(dict(
+                    id=message['author']['id'],
+                    name=cleaned_name
+                ))
+
+            if len(cleaned_message) < 1000:
+                personas.append(dict(
+                    id=message['author']['id'],
+                    persona=cleaned_message
+                ))
+
+            messages_processed+=1
+        
+        # Insert in bulk
+        db["names_temp"].insert_many(names, chunk_size=50_000)
+        db["personas_temp"].insert_many(personas, chunk_size=50_000)
+
+    return messages_processed
+
+def cache_messages(path):
+    messages_processed = 0
+    names = []
+    msgs = []
+
+    with open(os.path.join(data_folder, "content", path), "r") as f, dataset.connect(db_url) as db:
+        messages = ijson.items(f, 'messages.item')
+        for message in messages:
+            # Ignore if content is:
+            # - too long
+            # - has an embed
+            # - is a non-default/reply message
+            # - author is a bot
+            # - author is a "Deleted User"
+            # - author's name is too long
+            if (len(message["content"]) > 900 or
+                len(message["content"]) <= 1 or
+                message["embeds"] != [] or
+                message["type"] not in ["Default", "Reply"] or
+                message["author"]["isBot"] == True or
+                message["author"]["name"] == "Deleted User" or
+                len(message["author"]["name"]) > 100):
+                continue
+
+            cleaned_name = clean(message["author"]["name"])
+            cleaned_message = clean(message["content"])
+
+            if not (cleaned_message and cleaned_name):
+                continue
+
+            # Process in memory to increase speed
+            if len(cleaned_name) < 100:
+                names.append(dict(
+                    id=message['author']['id'],
+                    name=cleaned_name
+                ))
+
+            if len(cleaned_message) < 1000:
+                if message["type"] == "Reply":
+                    msgs.append(dict(
+                        id=message['id'],
+                        author_id=message['author']['id'],
+                        content=cleaned_message,
+                        refs=message["reference"]["messageId"]
+                    ))
+                else:
+                    msgs.append(dict(
+                        id=message['id'],
+                        author_id=message['author']['id'],
+                        content=cleaned_message
+                    ))
+
+            messages_processed+=1
+
+        # Insert in bulk
+        db["names_temp"].insert_many(names, chunk_size=100_000)
+        db["messages_temp"].insert_many(msgs, chunk_size=100_000)
+
+    return messages_processed
+
+def create_dataset(c):
+    db = dataset.connect(db_url)
+
+    messages_processed = 0
+    pairs = []
+    pairs_nopersona = []
+
+    # There is only one set of messages and one set of references.
+    # However, there can be multiple personas and author names per yuser, and we must account for that
+    messages = list(c)
+    references = {i["id"]:{"name": i["author_id"], "content": i["content"]} for i in db["messages"].find(id={"in": [message["refs"] for message in messages]})}
+    personas = {}
+    reference_author_names = {}
+
+    # Generate this thread's persona cache
+    for persona in db["personas"].find(id={"in": [message["author_id"] for message in messages]}):
+        if persona["id"] in personas:
+            personas[persona["id"]].append(persona["persona"])
+        else:
+            personas[persona["id"]] = [persona["persona"]]
+
+    # Generate this thread's references cache
+    for name in db["names"].find(id={"in":[references[message]["name"] for message in references]}):
+        if name["id"] in reference_author_names:
+            reference_author_names[name["id"]].append(name["name"])
+        else:
+            reference_author_names[name["id"]] = [name["name"]]
+
+    for message in messages:
+        messages_processed += 1
+
+        try:
+            context = references[message["refs"]]
+            names = reference_author_names[context["name"]]
+        except KeyError:  continue
+
+        try:
+            persona = personas[message["author_id"]]
+            for p in persona:
+                for name in names:
+                    pairs.append(dict(
+                        persona = p,
+                        context = f"{name}: {context['content']}",
+                        target = message['content']
+                    ))
+        except KeyError: pass
+
+        for name in names:
+            pairs_nopersona.append(dict(
+                context = f"{name}: {context['content']}",
+                target = message['content']
+            ))
+
+    # Insert in bulk
+    db["dataset"].insert_many(pairs, chunk_size=100_000)
+    db["dataset_nopersona"].insert_many(pairs_nopersona, chunk_size=100_000)
+
+    return messages_processed
+
+
+if __name__ == "__main__":
+    db = dataset.connect(db_url)
+
+    # Create status table
+    db.query("""
+    CREATE TABLE IF NOT EXISTS status (
+        ref VARCHAR(100) PRIMARY KEY,
+        ok BOOL
+    )
+    """)
+
+    status = db['status']
+
+    # Process and cache data
+    if not (table_ok("names") and table_ok("personas")):
+        status.upsert_many([
+            dict(
+                ref = "personas",
+                ok = False
+            ),
+            dict(
+                ref = "names",
+                ok = False
+            )
+        ], ["ref"])
+
+        db.query("""
+        DROP TABLE IF EXISTS personas_temp;
+        DROP TABLE IF EXISTS personas;
+        DROP TABLE IF EXISTS names_temp;
+        ALTER TABLE IF EXISTS names RENAME TO names_temp;
+
+        CREATE TABLE personas_temp (
+            id BIGINT NOT NULL,
+            persona VARCHAR(1000) NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS names_temp (
+            id BIGINT NOT NULL,
+            name VARCHAR(100) NOT NULL
+        );
+        """)
+
+        # One-pass, no constraint processing of intro data
+        tasks = os.listdir(os.path.join(data_folder, "intros"))
+        with concurrent.futures.ProcessPoolExecutor(max_workers=processing_cores) as executor:
+            total = list(tqdm(executor.map(cache_users, tasks), total=len(tasks), desc="Caching users..."))
+        
+        # Let sql handle uniqueness processing
+        db.query("SELECT DISTINCT * INTO personas FROM personas_temp;")
+        db.query("DROP TABLE personas_temp;")
+        db.query("SELECT DISTINCT * INTO names FROM names_temp;")
+        db.query("DROP TABLE names_temp;")
+
+        # Print some stats about the work that just happened
+        print(f"""
+        personas: {db["personas"].count()}
+        names: {db["names"].count()}
+        messages processed: {sum(total)}
+        """)
+
+        status.upsert_many([
+            dict(
+                ref = "personas",
+                ok = True
+            ),
+            dict(
+                ref = "names",
+                ok = True
+            )
+        ], ["ref"])
+    else:
+        print("Persona data OK. Skipping.")
+
+    if not table_ok("messages"):
+        status.upsert(dict(
+            ref = "messages",
+            ok = False
+        ), ["ref"])
+
+        db.query("""
+        DROP TABLE IF EXISTS messages_temp;
+        DROP TABLE IF EXISTS messages;
+        DROP TABLE IF EXISTS names_temp;
+        ALTER TABLE IF EXISTS names RENAME TO names_temp;
+
+        CREATE TABLE messages_temp (
+            id BIGINT NOT NULL,
+            author_id BIGINT NOT NULL,
+            refs BIGINT,
+            content VARCHAR(1000) NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS names_temp (
+            id BIGINT NOT NULL,
+            name VARCHAR(100) NOT NULL
+        );
+        """)
+
+        # One-pass, no constraint processing of message data
+        tasks = os.listdir(os.path.join(data_folder, "content"))
+        # for t in tqdm(tasks):
+        #     cache_messages(t)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=processing_cores) as executor:
+            total = list(tqdm(executor.map(cache_messages, tasks), total=len(tasks), desc="Caching messages..."))
+        
+        # Let sql handle uniqueness processing
+        db.query("SELECT DISTINCT * INTO names FROM names_temp;")
+        db.query("DROP TABLE names_temp;")
+        db.query("SELECT DISTINCT * INTO messages FROM messages_temp;")
+        db.query("DROP TABLE messages_temp")
+
+        # Print some stats about the work that just happened
+        print(f"""
+        messages: {db["messages"].count()}
+        names: {db["names"].count()}
+        messages processed: {sum(total)}
+        """)
+
+        status.upsert(dict(
+            ref = "messages",
+            ok = True
+        ), ["ref"])
+    else:
+        print("Message and Name data OK. Skipping.")
+
+    messages = db["messages"]
+
+    db.query("DROP TABLE IF EXISTS dataset;")
+    db.query("DROP TABLE IF EXISTS dataset_nopersona;")
+
+    tasks, total_tasks = chunk(messages.find(refs={"not": None}),  messages.count(refs={"not": None}), processing_cores*8)
+    # total = []
+    # for t in tqdm(tasks, total=total_tasks):
+    #     total.append(create_dataset(t))
+    with concurrent.futures.ProcessPoolExecutor(max_workers=processing_cores) as executor:
+        total = list(tqdm(executor.map(create_dataset, tasks), total=total_tasks, desc="Creating dataset..."))
+
+    print(f"""
+    with persona: {db["dataset"].count()}
+    no persona: {db["dataset_nopersona"].count()}
+    expected: {messages.count(refs={"not": None})}
+    processed: {sum(total)}
+    """)
+
+    # for message in tqdm(messages.find(refs={"not": None}), total = messages.count(refs={"not": None}), desc="Creating dataset..."):
+    #     # Gather info about the current message;
+    #     message = dict(message)
+    #     author_personas = [i["persona"] for i in personas.find(id=message["author_id"])]
+
+    #     # Gather info about the reference message
+    #     reference = messages.find_one(id=message["refs"])
+    #     reference = dict(reference) if reference else None
+    #     reference_author_names = [i["name"] for i in names.find(id=reference["author_id"])] if reference else []
+
+    #     for persona in author_personas:
+    #         for name in reference_author_names:
+    #             pairs.append(dict(
+    #                 persona = persona,
+    #                 context = f"{name}: {reference['content']}",
+    #                 target = message['content']
+    #             ))
+    #             kept+=1
+
+    #     for name in reference_author_names:
+    #         pairs_nopersona.append(dict(
+    #             context = f"{name}: {reference['content']}",
+    #             target = message['content']
+    #         ))
+    #         kept_nopersona+=1
+
+    #     total += 1
---
+++ src/helpers.py
@@ -0,0 +1,72 @@
+import re, io, json, random
+
+normalize_chars={'Š':'S', 'š':'s', 'Ð':'Dj','Ž':'Z', 'ž':'z', 'À':'A', 'Á':'A', 'Â':'A', 'Ã':'A', 'Ä':'A',
+    'Å':'A', 'Æ':'A', 'Ç':'C', 'È':'E', 'É':'E', 'Ê':'E', 'Ë':'E', 'Ì':'I', 'Í':'I', 'Î':'I',
+    'Ï':'I', 'Ñ':'N', 'Ń':'N', 'Ò':'O', 'Ó':'O', 'Ô':'O', 'Õ':'O', 'Ö':'O', 'Ø':'O', 'Ù':'U', 'Ú':'U',
+    'Û':'U', 'Ü':'U', 'Ý':'Y', 'Þ':'B', 'ß':'Ss','à':'a', 'á':'a', 'â':'a', 'ã':'a', 'ä':'a',
+    'å':'a', 'æ':'a', 'ç':'c', 'è':'e', 'é':'e', 'ê':'e', 'ë':'e', 'ì':'i', 'í':'i', 'î':'i',
+    'ï':'i', 'ð':'o', 'ñ':'n', 'ń':'n', 'ò':'o', 'ó':'o', 'ô':'o', 'õ':'o', 'ö':'o', 'ø':'o', 'ù':'u',
+    'ú':'u', 'û':'u', 'ü':'u', 'ý':'y', 'ý':'y', 'þ':'b', 'ÿ':'y', 'ƒ':'f',
+    'ă':'a', 'î':'i', 'â':'a', 'ș':'s', 'ț':'t', 'Ă':'A', 'Î':'I', 'Â':'A', 'Ș':'S', 'Ț':'T',}
+alphabets=io.open("src/alphabets.txt", mode="r", encoding="utf-8").read().strip().split("\n")
+badwords=io.open("src/badwords.txt", mode="r", encoding="utf-8").read().strip().split("\n")
+emojis=json.load(io.open("src/emojis.json", mode="r", encoding="utf-8"))
+for alphabet in alphabets[1:]:
+    for ind, char in enumerate(alphabet):
+        try:normalize_chars[char]=alphabets[0][ind]
+        except: print(alphabet, len(alphabet), len(alphabets[0]));break
+normalize_chars.update({i:i for i in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'})
+
+normal_map=str.maketrans(normalize_chars)
+del normalize_chars
+
+bot_prefixes=tuple(io.open("src/prefixes.txt", mode="r", encoding="utf-8").read().strip().split("\n"))
+names=io.open("src/names.txt", mode="r", encoding="utf-8").read().strip().split("\n")
+replace_names={}
+
+def gen_name(username):
+    try:
+        int(username)
+        try: out_name=replace_names[username]
+        except: 
+            out_name=random.choice(names)
+            replace_names[username]=out_name
+        return out_name
+    except: return " @"+random.choice(names)
+
+def convemojis(i):
+    if i in emojis: return emojis[i]
+    return i
+
+#precompile regex
+r1=re.compile(r'@Deleted User')
+r2=re.compile(r'https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=\n]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)|:[^\n\s]+?:|[\w\-\.]+@(?:[\w-]+\.)+[\w-]{2,4}|(?:\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}|```.+?```\n?|(?:\\n)+|\b(?:a*ha+h[ha]*|o?l+o+l+[ol]*)\b|[^a-z0-9.,:;\'\”@!?\s\<\>\/\-\+\=\(\)\[\]*_'+''.join(emojis)+r']+|(?<=[a-z.,\':;!?\/]) +(?=[.,\'!?\/])|([,\':;\s\/\(\)\[\]\+\-\<\>\=])\1+|([_])\2{2,}|([a-z.!?*])\3{3,}|(: )(?:> (?:.*?)(?:\n+|\\n+|$))+', flags=re.DOTALL | re.IGNORECASE)
+r3=re.compile(r'[\U00003000\U0000205F\U0000202F\U0000200A\U00002000-\U00002009\U00001680\U000000A0\t]+| {2,}')
+r4=re.compile(r"(.{3,})\1", re.IGNORECASE | re.DOTALL)
+
+def clean(text, author=None, author_id=False):
+    if text.lower() == "welc" or ("welcome" in text.lower()): return None #welcome is the bane of exisitence and needs to be culled
+    if "@everyone" in text.lower() or "@here" in text.lower(): return None #no need for these kinds of pings, and messages in them are even more useless.
+    if text.strip().lower().startswith(bot_prefixes): return None #handle bot commands
+
+    if author and author_id:
+        author = author.replace(":", "").strip()
+        if author == "Deleted User" or " " in author:
+            author = gen_name(author_id)
+        text = f"{author.strip()}: {text.strip()}"
+    
+    text=text.translate(normal_map)#handle special chars from other langs
+    text= re.sub(r1, gen_name, text.strip()) #replace "deleted users" with names
+    text= re.sub(r2, r"\1\2\2\3\3\3\4", text.strip()) #remove urls, emails, code blocks, custom emojis, non-emoji, punctuation, letters, and phone numbers
+    text= re.sub(r3, " ", text.strip()) #handle... interesting spaces
+    text= "".join(list(map(convemojis,text.strip()))) #translate emojis to their `:text:` shorthand form
+    text= "\n".join([ln.strip().strip("\t") for ln in text.split("\n")]) #handle newlines
+    
+    if text.startswith(": ") and author != None: text=gen_name(author)+text
+
+    for badword in badwords:
+        if badword.lower().strip() in text.lower(): return None
+
+    if not (text[text.find(':')+1:].strip() in ["", "\n", " ", "\t"] or text[text.find(':')+1:].strip().lower().startswith(bot_prefixes)): 
+        return text.lstrip(("!.,^#")).strip().replace("\t", " ")
+    else: return None
