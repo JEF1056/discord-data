3530387eb24cb8461799ee00a5b8e35c96507aaa
Jess Fan <jessefan2002@gmail.com>
2022-08-28 18:40:17 -0700

    update train args

--- train.sh
+++ train.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+# args tuned for RTX 2060 6gb, Ryzen 7 4800H
 python3 train.py \
     --model_name_or_path t5-small \
     --do_train \
@@ -5,14 +8,19 @@ python3 train.py \
     --train_file dataset_train.json \
     --validation_file dataset_validation.json \
     --output_dir tst-model \
-    --overwrite_output_dir \
-    --per_device_train_batch_size=4 \
-    --per_device_eval_batch_size=4 \
+    --per_device_train_batch_size=10 \
+    --per_device_eval_batch_size=10 \
     --learning_rate 3e-5 \
     --num_train_epochs 2 \
     --max_seq_length 512 \
+    --max_answer_length 128 \
     --predict_with_generate \
     --context_column persona \
     --question_column question \
     --answer_column answers \
-    --overwrite_cache True
+    --preprocessing_num_workers 14 \
+    --save_total_limit 3 \
+    --save_steps 2500 \
+    --logging_steps 500 \
+    --half_precision_backend apex \
+    --fp16 True
